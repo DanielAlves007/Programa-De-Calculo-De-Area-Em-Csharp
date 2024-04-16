4f3c6e3439d5b96153fa55725e82143bc07775b2
Daniel <130700853+DanielAlves007@users.noreply.github.com>
2024-04-16 12:13:20 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Beecrowd1002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14159, r;
+
+            r = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("A=" + (pi * (r * r)).ToString("0.0000"));
+        }
+    }
+}
